dcd4a13c5d2d18bbf86b5a8edf57a516a65b5228
jonghyeon <whdgus003@gmail.com>
2022-11-08 23:57:45 +0900

    tailwind css, apollo server, react-router setup

---
+++ src/routers/logged-in-router.tsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export const LoggedInRouter = () => <span>'Logged In'</span>;
---
+++ src/routers/logged-out-router.tsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export const LoggedOutRouter = () => <span>'Logged Out'</span>;
