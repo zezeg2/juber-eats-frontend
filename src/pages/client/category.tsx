6051d42195397d1d168bd801d18cdfa44c11bdd9
jonghyeon <whdgus003@gmail.com>
2022-12-01 00:47:27 +0900

    refactoring(split restaurant page with components)

--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,9 +1,13 @@
-import React, { useEffect } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import { category, categoryVariables } from '../../__api__/category';
 import { SearchForm } from '../../components/search-form';
+import { Paragraph } from '../../components/paragraph';
+import { Helmet } from 'react-helmet-async';
+import { Restaurant } from '../../components/restaurant';
+import { PageNavigator } from '../../components/page-navigator';
 
 const CATEGORY_QUERY = gql`
   query category($input: GetCategoryInput!) {
@@ -30,19 +34,53 @@ interface ICategoryParams {
 
 export const Category = () => {
   const { slug } = useParams<ICategoryParams>();
+  const [page, setPage] = useState(1);
   const { data, loading } = useQuery<category, categoryVariables>(
     CATEGORY_QUERY,
     {
       variables: {
-        input: { page: 1, slug },
+        input: { page, slug },
       },
     },
   );
 
-  console.log(data);
+  let restaurants;
+  if (data?.getCategoryBySlug.result?.restaurants) {
+    restaurants = data.getCategoryBySlug.result.restaurants;
+  }
   return (
     <div>
+      <Helmet>
+        <title>Category:{slug} | Juber Eats</title>
+      </Helmet>
       <SearchForm />
+      <div className="layout">
+        <Paragraph
+          title={`${
+            data ? data.getCategoryBySlug.result?.name : ''
+          } delivery in Province`}
+        />
+        <hr className="my-10" />
+        <div className="grid lg:grid-cols-2">
+          {restaurants
+            ? restaurants.map((restaurant) => (
+                <Restaurant
+                  key={restaurant.id}
+                  id={restaurant.id + ''}
+                  coverImage={restaurant.coverImage}
+                  name={restaurant.name}
+                  address={restaurant.address}
+                  categoryName={restaurant.category?.name}
+                />
+              ))
+            : undefined}
+        </div>
+        <PageNavigator
+          totalPage={data ? data.getCategoryBySlug.totalPage : null}
+          page={page}
+          setPage={setPage}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -4,8 +4,6 @@ import {
   restaurantsPageQuery,
   restaurantsPageQueryVariables,
 } from '../../__api__/restaurantsPageQuery';
-import { Restaurant } from '../../components/restaurant';
-import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import { SearchForm } from '../../components/search-form';
@@ -16,6 +14,8 @@ import {
   PROVIDENCE_FOOD_DELIVERY_CONTENT,
 } from '../../paragraphs';
 import { PageNavigator } from '../../components/page-navigator';
+import { RestaurantList } from '../../components/restaurant-list';
+import { CategoryList } from '../../components/category-list';
 
 const RESTAURANTS_QUERY = gql`
   query restaurantsPageQuery($input: AllRestaurantsInput!) {
@@ -50,13 +50,14 @@ export const Restaurants = () => {
       },
     },
   });
+
   return (
     <div>
       <Helmet>
         <title>Home : Juber Eats</title>
       </Helmet>
       <SearchForm />
-      <div className="mx-auto w-full max-w-xl md:max-w-screen-2xl">
+      <div className="layout">
         <Paragraph
           title={PROVIDENCE_FOOD_DELIVERY}
           content={PROVIDENCE_FOOD_DELIVERY_CONTENT}
@@ -64,38 +65,12 @@ export const Restaurants = () => {
         <hr className="my-10" />
         <Paragraph title={EXPLORE_BY_CATEGORY} />
         {!loading && (
-          <div className="content-center justify-center pb-20">
-            <div className="ml-16 sm:m-0">
-              {data?.allCategories.categories?.map((category) => (
-                <Link to={`/category/${category.slug}`}>
-                  <div
-                    key={category.id}
-                    className="m-4 inline-flex h-20 w-40 cursor-pointer rounded-lg bg-orange-100 p-2 text-lg shadow hover:bg-lime-100"
-                  >
-                    {category.name}
-                    <img
-                      className="w-20"
-                      src={
-                        category.coverImage ? category.coverImage : undefined
-                      }
-                    />
-                  </div>
-                </Link>
-              ))}
-            </div>
+          <div className="content-layout">
+            <CategoryList
+              categories={data ? data.allCategories.categories : null}
+            />
             <hr className="my-10" />
-            <div className="grid lg:grid-cols-2">
-              {data?.allRestaurants.result?.map((restaurant) => (
-                <Restaurant
-                  key={restaurant.id}
-                  id={restaurant.id + ''}
-                  coverImage={restaurant.coverImage}
-                  name={restaurant.name}
-                  address={restaurant.address}
-                  categoryName={restaurant.category?.name}
-                />
-              ))}
-            </div>
+            <RestaurantList result={data ? data.allRestaurants.result : null} />
             <PageNavigator
               totalPage={data ? data.allRestaurants.totalPage : null}
               page={page}
